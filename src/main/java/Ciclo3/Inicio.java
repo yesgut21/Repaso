853e5b91cf5079db4ed448c5a01b9e04cc95af40
YESID G <yesgut21@gmail.com>
2022-08-13 10:41:39 -0500

    Comit inicial del proyecto cambio1

--- src/main/java/Ciclo3/Inicio.java
+++ src/main/java/Ciclo3/Inicio.java
@@ -6,6 +6,7 @@ public class Inicio {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Bienvenidos a git");
+        System.out.println("Crear rama modalida movil");
 
     }
 }
