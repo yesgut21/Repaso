a125deba81f8fedd60b2abb15d5b9cb188d4f898
YESID G <yesgut21@gmail.com>
2022-08-13 10:23:36 -0500

    Comit inicial del proyecto cambio1

--- src/main/java/Ciclo3/Inicio.java
+++ src/main/java/Ciclo3/Inicio.java
@@ -5,5 +5,7 @@ import java.util.Arrays;
 public class Inicio {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Bienvenidos a git");
+
     }
 }
